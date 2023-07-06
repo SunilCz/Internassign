42f391aa6e74059684ade19a7380659a64b5946a
SunilCz <sunil.chaudhary7281@gmai.com>
2023-07-06 09:03:43 +0545

    done1

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,53 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+
 const Home = () => {
-  const [data,setData] = useState([])
-  const [filter,setFilter] = useState(data)
-  const [loading , setLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
   let componentMounted = true;
+
   useEffect(() => {
     const getProduct = async () => {
       setLoading(true);
       const response = await fetch("https://fakestoreapi.com/products");
-      if(componentMounted){
-        setData( await response.clone().json());
-        setFilter(await response.json())
+      if (componentMounted) {
+        const responseData = await response.clone().json();
+        setData(responseData);
+        setFilter(responseData);
         setLoading(false);
-        console.log(filter)
+        console.log(filter);
       }
-      
     };
     getProduct();
+
+    return () => {
+      componentMounted = false;
+    };
   }, []);
-  
-  const filterhandle = (cat) =>{
-    const updateList = data.filter((x)=> x.category === cat)
-    setFilter(updateList)
-  }
+
+  const filterHandle = (category) => {
+    const updatedList = data.filter((item) => item.category === category);
+    setFilter(updatedList);
+  };
 
   const productList = filter.map((product) => {
-    const src = `${product.image}`
+    const src = `${product.image}`;
     return (
-  
-      <div className="col-md-4 mb-4">
-        <div className="card h-100 text-center p-4" key={product.id}>
+      <div className="col-md-4 mb-4" key={product.id}>
+        <div className="card h-100 text-center p-4">
           <div className="card-image">
-            <Image loader={()=> src} src={src} alt={product.title} height="300" width="300"/>
+            <Image loader={() => src} src={src} alt={product.title} height="300" width="300" />
           </div>
           <div className="card-body">
-            <h5 className="card-title"><b>{product.title.substring(0,11)}</b></h5>
+            <h5 className="card-title"><b>{product.title.substring(0, 11)}</b></h5>
             <p className="card-text"><b>${product.price}</b></p>
-           <div className="card-action">
-            <Link legacyBehavior href={`/product/${product.id}`}>
-            <a className="btn btn-primary ">Details</a>
-          </Link>
-          </div>
+            <div className="card-action">
+              <Link legacyBehavior href={`/product/${product.id}`}>
+                <a className="btn btn-primary">Details</a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
@@ -50,22 +55,20 @@ const Home = () => {
   });
 
   return (
-  <>
-  <div className="button center-align" style={{margin:"20px"}}>
-  <button onClick={()=> setFilter(data)} style={{margin:"3px"}}> All </button>
-  <button onClick={()=> filterhandle("men's clothing")} style={{margin:"3px"}}> Men Clothing</button>
-  <button onClick={()=> filterhandle("women's clothing")} style={{margin:"3px"}}> Women Clothing</button>
-  <button onClick={()=> filterhandle("jewelery")} style={{margin:"3px"}}> Jewelery</button>
-  <button onClick={()=> filterhandle("electronics")} style={{margin:"3px"}}> Electronics</button>
-  </div>
-  
-  <div className="rootcard">
-    {productList}
-    </div>
- 
-  </>
+    <>
+      <div className="button center-align" style={{ margin: "20px" }}>
+        <button onClick={() => setFilter(data)} style={{ margin: "3px" }}>All</button>
+        <button onClick={() => filterHandle("men's clothing")} style={{ margin: "3px" }}>Men Clothing</button>
+        <button onClick={() => filterHandle("women's clothing")} style={{ margin: "3px" }}>Women Clothing</button>
+        <button onClick={() => filterHandle("jewelery")} style={{ margin: "3px" }}>Jewelery</button>
+        <button onClick={() => filterHandle("electronics")} style={{ margin: "3px" }}>Electronics</button>
+      </div>
+
+      <div className="rootcard">
+        {productList}
+      </div>
+    </>
   );
 };
-
 
 export default Home;
